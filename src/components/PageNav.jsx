c0760c7839a0885f1805f34a2f4912c19bef9d53
LiZhe <1756368629@qq.com>
2024-03-18 20:26:25 -0400

    Enhance Navigation Item Visibility with Active State and Hover Effects

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 function PageNav() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  // Function to generate the common classes for NavLink, with additional classes based on isActive
+  const linkClass = (isActive) =>
+    `${
+      isActive
+        ? 'border-gray-600 bg-gray-200'
+        : 'hover:border-b-2 hover:border-gray-400'
+    } block py-2 px-4 w-full text-center font-semibold transition duration-300`;
 
   return (
     <div>
@@ -15,66 +23,31 @@ function PageNav() {
             className="md:hidden px-2 py-1 text-gray-800"
             onClick={() => setIsNavExpanded(!isNavExpanded)}
           >
-            {isNavExpanded ? "✕" : "☰"}
+            {isNavExpanded ? '✕' : '☰'}
           </button>
           <div
             className={`absolute z-10 left-0 mt-1 w-full rounded-lg shadow-md bg-white ${
-              isNavExpanded ? "block" : "hidden"
+              isNavExpanded ? 'block' : 'hidden'
             } md:block md:static md:shadow-none md:bg-transparent md:mt-0 md:w-auto md:rounded-none`}
           >
             <ul className="block md:flex items-center space-x-0 md:space-x-10 py-2 md:py-0">
-              <li className="w-full">
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    isActive
-                      ? "block py-2 px-4 w-full text-center bg-gray-200 rounded-md border-4 border-gray-600 font-semibold md:border-none md:rounded-none md:hover:bg-gray-200"
-                      : "block py-2 px-4 w-full text-center text-gray-600 font-semibold hover:bg-gray-200 md:hover:bg-gray-200 transition duration-300 md:hover:border-none"
-                  }
-                  onClick={() => setIsNavExpanded(false)}
-                >
-                  HomePage
-                </NavLink>
-              </li>
-              <li className="w-full">
-                <NavLink
-                  to="/projects"
-                  className={({ isActive }) =>
-                    isActive
-                      ? "block py-2 px-4 w-full text-center bg-gray-200 rounded-md border-4 border-gray-600 font-semibold md:border-none md:rounded-none md:hover:bg-gray-200"
-                      : "block py-2 px-4 w-full text-center text-gray-600 font-semibold hover:bg-gray-200 md:hover:bg-gray-200 transition duration-300 md:hover:border-none"
-                  }
-                  onClick={() => setIsNavExpanded(false)}
-                >
-                  Projects
-                </NavLink>
-              </li>
-              <li className="w-full">
-                <NavLink
-                  to="/coursework"
-                  className={({ isActive }) =>
-                    isActive
-                      ? "block py-2 px-4 w-full text-center bg-gray-200 rounded-md border-4 border-gray-600 font-semibold md:border-none md:rounded-none md:hover:bg-gray-200"
-                      : "block py-2 px-4 w-full text-center text-gray-600 font-semibold hover:bg-gray-200 md:hover:bg-gray-200 transition duration-300 md:hover:border-none"
-                  }
-                  onClick={() => setIsNavExpanded(false)}
-                >
-                  Coursework
-                </NavLink>
-              </li>
-              <li className="w-full">
-                <NavLink
-                  to="/about"
-                  className={({ isActive }) =>
-                    isActive
-                      ? "block py-2 px-4 w-full text-center bg-gray-200 rounded-md border-4 border-gray-600 font-semibold md:border-none md:rounded-none md:hover:bg-gray-200"
-                      : "block py-2 px-4 w-full text-center text-gray-600 font-semibold hover:bg-gray-200 md:hover:bg-gray-200 transition duration-300 md:hover:border-none"
-                  }
-                  onClick={() => setIsNavExpanded(false)}
-                >
-                  About
-                </NavLink>
-              </li>
+              {['/', '/projects', '/coursework', '/about'].map(
+                (path, index) => (
+                  <li className="w-full" key={index}>
+                    <NavLink
+                      to={path}
+                      className={({ isActive }) =>
+                        `${linkClass(isActive)} ${
+                          isActive ? 'border-b-2' : 'text-gray-600'
+                        }`
+                      }
+                      onClick={() => setIsNavExpanded(false)}
+                    >
+                      {path === '/' ? 'HomePage' : path.substring(1)}
+                    </NavLink>
+                  </li>
+                )
+              )}
             </ul>
           </div>
         </div>
